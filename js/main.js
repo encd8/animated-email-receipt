7359cb146cc649c9dbcf6185eeffbbab4316abac
Abhisek <abhisek.dutt@gmail.com>
2018-08-29 16:54:36 +0530

    Started working on animation

---
+++ js/main.js
@@ -0,0 +1,14 @@
+tl = anime.timeline();
+
+tl.add({
+	targets: "#topbar",
+	width:[0,"50px"],
+	height:[0,"72px"],
+});
+
+tl.add({
+	targets: "#topbar",
+	opacity:[0,1],
+	translateY: ["200px", 0]
+})
+
