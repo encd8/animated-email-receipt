9311ba418f68cf00e56e2be57d20cc199145adba
--global <email@encode8.com>
2018-08-30 08:22:04 +0530

    Animation (almost) complete. Need some cosmetic changes

--- js/main.js
+++ js/main.js
@@ -1,14 +1,41 @@
-tl = anime.timeline();
+document.addEventListener("DOMContentLoaded", function(event) {
 
-tl.add({
-	targets: "#topbar",
-	width:[0,"50px"],
-	height:[0,"72px"],
+	var animTimeline = new anime.timeline({});
+
+	animTimeline.add({
+		targets: '#apple-logo',
+		width: [0, '72px'],
+		height: [0, '72px'],
+	})
+
+	animTimeline.add({
+		targets: '#inner',
+		maxWidth: '450px',
+	})
+
+	animTimeline.add({
+		targets: '#date',
+		easing: 'easeOutQuad',
+		duration: 200,
+		scale: [0,1],
+		translateX: ['-8px', '0px']
+	})
+
+	animTimeline.add({
+		targets: '#details',
+		rotateX: ["90deg", "0deg"],
+	})
+
+	animTimeline.add({
+		targets: '#barcode',
+		rotateX: ['90deg', '0deg']
+	})
+
+	animTimeline.add({
+		targets: '.row-anim',
+		opacity: [0,1],
+		translateY: ['3px','0px'],
+		delay: function(el, i) { return (i * 200); },
+	})
+
 });
-
-tl.add({
-	targets: "#topbar",
-	opacity:[0,1],
-	translateY: ["200px", 0]
-})
-
